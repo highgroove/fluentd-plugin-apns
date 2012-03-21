f10a0f948ef3858e21ab3fa7c72d72634c7dac64
vanstee <vanstee@highgroove.com>
2012-03-20 21:35:30 -0400

    Remove extra generated files

--- lib/fluentd-plugin-apns.rb
+++
@@ -1,9 +0,0 @@
-require "fluentd-plugin-apns/version"
-
-module Fluentd
-  module Plugin
-    module Apns
-      # Your code goes here...
-    end
-  end
-end
--- lib/fluentd-plugin-apns/version.rb
+++
@@ -1,7 +0,0 @@
-module Fluentd
-  module Plugin
-    module Apns
-      VERSION = "0.0.1"
-    end
-  end
-end
